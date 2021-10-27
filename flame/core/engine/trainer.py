8fae9474c6e2e2c0257004e75428019d8771c4e2
Pham Xuan Phong <86842861+PhongPX1603@users.noreply.github.com>
2021-10-27 14:55:35 +0700

    Update trainer.py

--- flame/core/engine/trainer.py
+++ flame/core/engine/trainer.py
@@ -42,7 +42,7 @@ class Trainer(Engine):
         super(Trainer, self).__init__(dataset, device, max_epochs)
 
     def init(self):
-        assert 'model' in self.frame, 'The frame does not have model.'
+        assert 'model' in self.frame, 'The frame does not have model.'      # check mode, optimizer and loss functions in frame is exist or not
         assert 'optim' in self.frame, 'The frame does not have optim.'
         assert 'loss' in self.frame, 'The frame does not have loss.'
         self.model = self.frame['model'].to(self.device)
@@ -50,22 +50,22 @@ class Trainer(Engine):
         self.loss = self.frame['loss']
 
     def _update(self, engine, batch):
-        self.model.train()
-        self.optimizer.zero_grad()
-        params = [param.to(self.device) if torch.is_tensor(param) else param for param in batch]
-        samples = torch.stack([sample.to(self.device) for sample in params[0]], dim=0)
-        targets = [{k: v.to(self.device) for k, v in target.items() if not isinstance(v, list)} for target in params[1]]
+        self.model.train()          # convert to mode train
+        self.optimizer.zero_grad()      # convert grad to 0 
+        params = [param.to(self.device) if torch.is_tensor(param) else param for param in batch]        # check params is tensor => to device
+        samples = torch.stack([sample.to(self.device) for sample in params[0]], dim=0)                  # stack all sample in batch to shape [batch, 3, h, w]
+        targets = [{k: v.to(self.device) for k, v in target.items() if not isinstance(v, list)} for target in params[1]]    # convert target to a list of dictionaries contain 
+                                                                                                                            # labels and bboxes
+        preds = self.model(samples)             # put samples into model => predicts are prediction boxes and labels
 
-        preds = self.model(samples)
+        losses = self.loss(preds, targets)      # compute loss between preds and targets
 
-        losses = self.loss(preds, targets)
+        loss = losses[13] + losses[26] + losses[52]     # total loss of 3 scales 
+        loss.backward()                         # compute backward
+        
+        self.optimizer.step()                   # implement optimizer
 
-        loss = losses[13] + losses[26] + losses[52]
-        loss.backward()
-
-        self.optimizer.step()
-
-        return loss.item()
+        return loss.item()                      # return value of loss
 
 
 class Evaluator(Engine):
@@ -79,10 +79,10 @@ class Evaluator(Engine):
 
     def init(self):
         assert 'model' in self.frame, 'The frame does not have model.'
-        self.model = self.frame['model'].to(self.device)
+        self.model = self.frame['model'].to(self.device)        # convert model to device 
 
     def _update(self, engine, batch):
-        self.model.eval()
+        self.model.eval()           # covert to mode evaluator
         with torch.no_grad():
             params = [param.to(self.device) if torch.is_tensor(param) else param for param in batch]
             samples = torch.stack([image.to(self.device) for image in params[0]], dim=0)
